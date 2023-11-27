676a5c677903558470bde08c79b1edb47379510a
geshan99 <geshansudasinghe@gmail.com>
2023-11-27 23:30:10 +0530

    #testing123

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -134,7 +134,7 @@ function Navbar() {
 
 			<Box>
 				<Button variant="solid" colorScheme="teal">
-					<a href="/blg.html">Blog</a>
+					<a href="/blog.html">Blog</a>
 				</Button>
 			</Box>
 		</Flex>
