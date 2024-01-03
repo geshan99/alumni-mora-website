d2e635c7a765ebcd6521b75c6a6c9708445c857f
Bhanuka Uyanage <50510335+BhanukaC@users.noreply.github.com>
2024-01-03 11:44:57 +0530

    Blog link changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -134,7 +134,7 @@ function Navbar() {
 
 			<Box>
 				<Button variant="solid" colorScheme="teal">
-					<a href="https://blog.codewithx.com/">Blog</a>
+					<a href="https://blog.rotaractalumnimora.org">Blog</a>
 				</Button>
 			</Box>
 		</Flex>
