24bae2032d70c50a03faa45850908f813e4b18b2
Bhanuka Uyanage <50510335+BhanukaC@users.noreply.github.com>
2024-04-27 22:55:57 +0530

    Update AnnualReportCard2.js

--- src/components/downloads/AnnualReportCard2.js
+++ src/components/downloads/AnnualReportCard2.js
@@ -6,7 +6,9 @@ function AnnualReportCard2({ title,year,title2, imageUrl, pdfUrl }) {
 		<Box borderWidth="1px" borderRadius="lg" overflow="hidden">
 			<Image src={imageUrl} alt={title} w={250} m={5} />
 			<Box p="4">
-				<Heading fontSize="xl">{title} <br />{year} <br />{title2}</Heading><br /><Heading fontSize="xl">{title2}</Heading>
+				<Heading fontSize="xl">{title}</Heading>
+				<Heading fontSize="xl">{year}</Heading>
+				<Heading fontSize="xl">{title2}</Heading>
 				<Button colorScheme="teal" mt="2" size="sm">
 					<a
 						href={pdfUrl}
